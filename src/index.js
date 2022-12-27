6e2c3d4ec9bae8acf76901a470b703da6940aed5
wallnnut <81454917+wallnnut@users.noreply.github.com>
2022-12-27 20:57:40 +0300

    getState, Dispatch

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function createStore(initialState) {
+	let state = initialState;
+	function getState() {
+		return state;
+	}
+	function dispatch(action) {
+		if (action.type === "task/completed") {
+			const newArray = new Array(state);
+			const index = newArray.findIndex(
+				(elem) => elem.id === action.payload.id
+			);
+
+			newArray[index].completed = true;
+			state = newArray;
+			console.log(state);
+		}
+	}
+	return { getState, dispatch };
+}
+const store = createStore({
+	id: 1,
+	description: "description",
+	completed: false,
+});
+const App = () => {
+	return (
+		<>
+			<button
+				onClick={() =>
+					store.dispatch({
+						type: "task/completed",
+						payload: { id: 1 },
+					})
+				}
+			>
+				Complete
+			</button>
+			<h1>App</h1>
+		</>
+	);
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
